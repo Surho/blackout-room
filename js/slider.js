2ebbe0fd1517d2dcc7f732f15bcada01b56cda66
Surho <surho007@hotmail.com>
2018-10-13 23:19:13 +0300

    slider fix4

--- js/slider.js
+++ js/slider.js
@@ -2,44 +2,39 @@ let currentWidth = document.documentElement.clientWidth;
 let sliderMode = false;
 
 $(document).ready(function() {
-  console.log("!!!!");
   $('.rooms__list').slick({
     centerMode: true,
     slidesToShow: 1,
-    arrows: false,
-    infinite: false
+    arrows: true,
+    infinite: true
   });
+
+  sliderMode = true;
+
+  if(currentWidth > 545) {
+    $('.rooms__list').slick('unslick');
+    sliderMode = false;
+  }
 });
 
 
 
+  window.addEventListener('resize', () => {
+    currentWidth = document.documentElement.clientWidth;
+    console.log(currentWidth);
+    if(currentWidth > 545 && sliderMode) {
+      $('.rooms__list').slick('unslick');
+      sliderMode = false;
+    } 
 
-//   sliderMode = true;
+    if(currentWidth < 545 && !sliderMode) {
+      $('.rooms__list').slick({
+        centerMode: true,
+        slidesToShow: 1,
+        arrows:true ,
+        infinite: true
+      });
+      sliderMode = true;
+    } 
 
-//   if(currentWidth > 545) {
-//     $('.rooms__list').slick('unslick');
-//     sliderMode = false;
-//   }
-// });
-
-
-
-//   window.addEventListener('resize', () => {
-//     currentWidth = document.documentElement.clientWidth;
-//     console.log(currentWidth);
-//     if(currentWidth > 545 && sliderMode) {
-//       $('.rooms__list').slick('unslick');
-//       sliderMode = false;
-//     } 
-
-//     if(currentWidth < 545 && !sliderMode) {
-//       $('.rooms__list').slick({
-//         centerMode: true,
-//         slidesToShow: 1,
-//         arrows: false,
-//         infinite: false
-//       });
-//       sliderMode = true;
-//     } 
-
-//   })
+  })
