33357e05a8889e6c4897b57b12e4dbe00e3f6c1f
Surho <surho007@hotmail.com>
2018-10-14 21:30:18 +0300

    fix try 8

--- js/slider.js
+++ js/slider.js
@@ -8,33 +8,33 @@ $(document).ready(function() {
     arrows: false,
     infinite: true
   });
-
-  sliderMode = true;
-
-  if(currentWidth > 545) {
-    $('.rooms__list').slick('unslick');
-    sliderMode = false;
-  }
 });
 
+//   sliderMode = true;
+
+//   if(currentWidth > 545) {
+//     $('.rooms__list').slick('unslick');
+//     sliderMode = false;
+//   }
+// });
 
 
-  window.addEventListener('resize', () => {
-    currentWidth = document.documentElement.clientWidth;
-    console.log(currentWidth);
-    if(currentWidth > 545 && sliderMode) {
-      $('.rooms__list').slick('unslick');
-      sliderMode = false;
-    } 
 
-    if(currentWidth < 545 && !sliderMode) {
-      $('.rooms__list').slick({
-        centerMode: true,
-        slidesToShow: 1,
-        arrows: false ,
-        infinite: true
-      });
-      sliderMode = true;
-    } 
+//   window.addEventListener('resize', () => {
+//     currentWidth = document.documentElement.clientWidth;
+//     if(currentWidth > 545 && sliderMode) {
+//       $('.rooms__list').slick('unslick');
+//       sliderMode = false;
+//     } 
 
-  })
+//     if(currentWidth < 545 && !sliderMode) {
+//       $('.rooms__list').slick({
+//         centerMode: true,
+//         slidesToShow: 1,
+//         arrows: false ,
+//         infinite: true
+//       });
+//       sliderMode = true;
+//     } 
+
+//   })
