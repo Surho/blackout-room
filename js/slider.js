a5c3d9a03932ffbeb215427cb886d3f12e1d39d0
Surho <surho007@hotmail.com>
2018-10-18 00:57:39 +0300

    breadcrumbs + next room

--- js/slider.js
+++ js/slider.js
@@ -1,46 +1,46 @@
   
-var screenWidth = document.documentElement.clientWidth;
-var sliderOn = true;
+// var screenWidth = document.documentElement.clientWidth;
+// var sliderOn = true;
 
 
-var swiper = new Swiper('.swiper-container', {
-  slidesPerView: '2',
-  centeredSlides: true,
-  spaceBetween: 10,
-  pagination: {
-    el: '.swiper-pagination',
-    clickable: true,
-  },
-});
+// var swiper = new Swiper('.swiper-container', {
+//   slidesPerView: '2',
+//   centeredSlides: true,
+//   spaceBetween: 10,
+//   pagination: {
+//     el: '.swiper-pagination',
+//     clickable: true,
+//   },
+// });
 
-if (screenWidth > 730) {
-  swiper.destroy(true, true);
-  sliderOn = false;
-  document.body.classList.add('slider-off');
-}
+// if (screenWidth > 730) {
+//   swiper.destroy(true, true);
+//   sliderOn = false;
+//   document.body.classList.add('slider-off');
+// }
 
-window.addEventListener('resize', () => {
-  screenWidth = document.documentElement.clientWidth;
+// window.addEventListener('resize', () => {
+//   screenWidth = document.documentElement.clientWidth;
 
-  if(screenWidth > 730 && sliderOn) {
-    swiper.destroy(false, true);
-    sliderOn = false;
-    document.body.classList.add('slider-off');
-  }
+//   if(screenWidth > 730 && sliderOn) {
+//     swiper.destroy(false, true);
+//     sliderOn = false;
+//     document.body.classList.add('slider-off');
+//   }
   
-  if(screenWidth < 730 && !sliderOn) {
-    swiper = new Swiper('.swiper-container', {
-      slidesPerView: '2',
-      centeredSlides: true,
-      spaceBetween: 10,
-      pagination: {
-        el: '.swiper-pagination',
-        clickable: true,
-      },
-    });
+//   if(screenWidth < 730 && !sliderOn) {
+//     swiper = new Swiper('.swiper-container', {
+//       slidesPerView: '2',
+//       centeredSlides: true,
+//       spaceBetween: 10,
+//       pagination: {
+//         el: '.swiper-pagination',
+//         clickable: true,
+//       },
+//     });
 
-    sliderOn = true;
-    document.body.classList.remove('slider-off');
-  }
-});
+//     sliderOn = true;
+//     document.body.classList.remove('slider-off');
+//   }
+// });
 
